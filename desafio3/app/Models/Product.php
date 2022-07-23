deb5640d02164acb71c5010cb4a1df68fee3b262
Josemar Gama <josemargama7@gmail.com>
2022-07-23 02:34:42 -0300

    resolucao_desafio3.2

--- desafio3/app/Models/Product.php
+++ desafio3/app/Models/Product.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Product extends Model
 {
@@ -14,6 +16,18 @@ class Product extends Model
         'category_id',
     ];
     protected $table = 'products';
+
+    public function categories()
+    {
+        return $this->hasOne(Category::class, 'category_id', 'id');
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class,
+            'category_id',
+            'id',
+            'id');
+    }
 
 
 }
